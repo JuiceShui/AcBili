7879f116ad5909ecad2e2a86328849ac26780632
JuiceShui <shuicz0505@qq.com>
2016-10-16 17:29:28 +0800

    //添加分享功能

--- app/src/main/java/acbili/shui/com/acbili/NewsDetailsWebActivity.java
+++ app/src/main/java/acbili/shui/com/acbili/NewsDetailsWebActivity.java
@@ -19,6 +19,8 @@ import acbili.shui.com.base.BaseActivity;
 import acbili.shui.com.bean.NewsModel;
 import acbili.shui.com.callBack.DialogCallback;
 import acbili.shui.com.global.Urls;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -33,6 +35,7 @@ public class NewsDetailsWebActivity extends BaseActivity implements OnClickListe
 	private Intent mIntent;
 	private String mTitle;
 	private String mData;
+	private String mURL;
 	private int mId;
 	private String TAG=Urls.URL_TOP+"show";
 
@@ -41,6 +44,7 @@ public class NewsDetailsWebActivity extends BaseActivity implements OnClickListe
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_news_details_webview );
+		ShareSDK.initSDK(this);
 		initView();
 		initData();
 	}
@@ -127,45 +131,38 @@ public class NewsDetailsWebActivity extends BaseActivity implements OnClickListe
 				finish();
 				break;
 			case R.id.btn_news_details_share:
-				//分享事件
+				showShare();
 				break;
 			default:
 				break;
 		}
 		
 	}
-	/**
-	 * 分享
-	 *//*
 	private void showShare() {
 		ShareSDK.initSDK(this);
 		OnekeyShare oks = new OnekeyShare();
-		// oks.setTheme(OnekeyShareTheme.SKYBLUE);//設置分享的主題
-		// 关闭sso授权
+		//关闭sso授权
 		oks.disableSSOWhenAuthorize();
 
-		// 分享时Notification的图标和文字 2.5.9以后的版本不调用此方法
-		// oks.setNotification(R.drawable.ic_launcher,
-		// getString(R.string.app_name));
-		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-		oks.setTitle(getString(R.string.share));
-		// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-		oks.setTitleUrl("http://sharesdk.cn");
+		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+		oks.setTitle("标题");
+		// titleUrl是标题的网络链接，QQ和QQ空间等使用
+		oks.setTitleUrl(mURL);
 		// text是分享文本，所有平台都需要这个字段
 		oks.setText("我是分享文本");
 		// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-		// oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+		//oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
 		// url仅在微信（包括好友和朋友圈）中使用
-		oks.setUrl("http://sharesdk.cn");
+		oks.setUrl(mURL);
 		// comment是我对这条分享的评论，仅在人人网和QQ空间使用
 		oks.setComment("我是测试评论文本");
 		// site是分享此内容的网站名称，仅在QQ空间使用
 		oks.setSite(getString(R.string.app_name));
 		// siteUrl是分享此内容的网站地址，仅在QQ空间使用
-		oks.setSiteUrl("http://sharesdk.cn");
+		oks.setSiteUrl(mURL);
 		// 启动分享GUI
 		oks.show(this);
-	}*/
+	}
 	private  class NewsDetailCallback extends DialogCallback<NewsModel.NewsContent>
 	{
 
@@ -185,6 +182,7 @@ public class NewsDetailsWebActivity extends BaseActivity implements OnClickListe
 		private  void  LayoutData(NewsModel.NewsContent newsContent)
 		{
 			mData=newsContent.message;
+			mURL=newsContent.fromurl;
 			//	tv_newsDetailTitle.setText(mTitle);
 			//下面的这个方法不能加载图片  只是转化为纯文本
 			//webView.loadDataWithBaseURL(null,Html.fromHtml(mData)+"", "text/html",  "utf-8", null);
